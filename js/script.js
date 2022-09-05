f7c05528f3f2bcde5dd2c26c825ab82759d248c8
tfrvld <tfrvld@gmail.com>
2022-09-05 20:06:30 +0700

    Menambahkan AOS dan GSAP

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const form = document.forms['contact-form'];
 const btnKirim = document.querySelector('.btn-kirim');
 const btnLoading = document.querySelector('.btn-loading');
 const myAlert = document.querySelector('.my-alert');
+const galleryImage = document.querySelectorAll('.gallery-img');
 
 form.addEventListener('submit', e => {
    e.preventDefault()
@@ -23,3 +24,22 @@ form.addEventListener('submit', e => {
       })
       .catch(error => console.error('Error!', error.message))
 })
+
+// AOS
+galleryImage.forEach((img, i) => {
+   img.dataset.aos = 'fade-down';
+   img.dataset.aosDelay = i * 150;
+   img.dataset.aosDuration = 1000;
+})
+
+AOS.init({
+   once: true,
+   duration: 1500
+});
+
+// GSAP
+gsap.registerPlugin(TextPlugin);
+gsap.to('.lead', { duration: 1.5, delay: 1.5, text: 'college student | Web Developer' });
+gsap.from('.jumbotron img', { duration: 1, y: -100, opacity: 0, delay: .5 });
+gsap.from('.navbar', { duration: 1.5, y: '-100%', opacity: 0 });
+gsap.from('.display-4', { duration: 1, x: -150, opacity: 0, delay: .25 });
